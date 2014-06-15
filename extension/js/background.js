4639c51586b929953d93cf375c7ca365e997850b
Varun Agrawal <varagrawal@gmail.com>
2014-06-15 22:19:47 +0530

    Fixed indentations

--- extension/js/background.js
+++ extension/js/background.js
@@ -6,25 +6,24 @@
 */
 
 (function() {
-    
-    var xhr = new XMLHttpRequest();
+
+	var xhr = new XMLHttpRequest();
 	var _url = "http://www.quora.com/api/logged_in_user?fields=inbox,notifs";
 	
-    var quora_json = "";
-    var name = "";
-    var inbox = "";
+	var quora_json = "";
+	var name = "";
+	var inbox = "";
 	var link = "";
 	var notifs = "";
+	var voteUp = "";
+	var answered = "";
+	var following = "";
+	var other = "";
     
-    var voteUp = "";
-    var answered = "";
-    var following = "";
-    var other = "";
-    
-    var POLLING_FREQUENCY = 3000;
-    var initialized = false;
+	var POLLING_FREQUENCY = 3000;
+	var initialized = false;
 	var timer_on = false;
-    var classification_done = false;
+	var classification_done = false;
 	
 	if(!initialized)
 	{
@@ -34,13 +33,12 @@
 		startPolling();
 		console.log("Quorator: Background page initialized successfully");
 	}
-    
 
     function startPolling() {
-        getData();
-        
-        setTimeout(startPolling, POLLING_FREQUENCY);
-    }
+		getData();
+		
+		setTimeout(startPolling, POLLING_FREQUENCY);
+	}
     
 
 	/* Retrieve all the Data from Quora */
@@ -62,10 +60,9 @@
 		
 	}
 
-    
     function classify() {
         
-        if(xhr.readyState != 4) 
+		if(xhr.readyState != 4) 
 			return;
 			
 		if(xhr.readyState == 4 && xhr.status == 200)
@@ -75,53 +72,52 @@
 			inbox = quora_json.inbox;
 			link = quora_json.link;
 			notifs = quora_json.notifs;
-            
-            chrome.browserAction.setBadgeText({'text': String(notifs['unseen_count'] + inbox['unread_count'])});
+			
+			chrome.browserAction.setBadgeText({'text': String(notifs['unseen_count'] + inbox['unread_count'])});
 			
 			name = quora_json.name;
 			
 			var unseen = notifs.unseen;
 			voteUp = "";
-            answered = "";
-            following = "";
-            other = "";
-            
+			answered = "";
+			following = "";
+			other = "";
+			
 			for(var i=0; i<unseen.length; i++) {
-                
-                unseen[i] = addTargetAttribute(unseen[i]);
-
+			
+				unseen[i] = addTargetAttribute(unseen[i]);
+			
 				unseen[i] = boldLinks(unseen[i]);
-
-
-                if(isVoteUp(unseen[i])){
+				
+				if(isVoteUp(unseen[i])){
 					voteUp = voteUp + unseen[i] + "<br/><br/>";
 				}
 				else if(isAnswered(unseen[i])){
 					answered = answered + unseen[i] + "<br/><br/>";
 				}
-				else if(isFollowing(unseen[i])){
-                
-                    /* Make the Follow link bold */
-                    var follow = />Follow[0-9]*</;
-                    var follow_highlight = '>| Follow | <'
-                    
-                    following = following + unseen[i].replace(follow, follow_highlight) + "<br/><br/>";
+				else if(isFollowing(unseen[i])){  
+				
+					/* Make the Follow link bold */
+					var follow = />Follow[0-9]*</;
+					var follow_highlight = '>| Follow | <'
+					
+					following = following + unseen[i].replace(follow, follow_highlight) + "<br/><br/>";
 				}
 				else{
 					other = other + unseen[i] + "<br/><br/>";
 				}
 			}
             
-            console.log("Quorator: Classification done!");
+			console.log("Quorator: Classification done!");
 			classification_done = true;
 		}
-    }
-    
-    function addTargetAttribute(str) {
-        var re = /<a /gi;
+	}
+	
+	function addTargetAttribute(str) {
+		var re = /<a /gi;
         
-        return str.replace(re, '<a target="_blank"');
-    }
+		return str.replace(re, '<a target="_blank"');
+	}
     
 	function boldLinks(str) {
 		var re = /<a.*?>.*?<\/a>/gi;
@@ -134,10 +130,10 @@
 		return str;
 		
 	}
-
-    function isFollowing(message) {
+	
+	function isFollowing(message) {
 		var re = /now following you/i;
-        
+		
 		return re.test(message);
 	}
 	
@@ -150,29 +146,30 @@
 	function isAnswered(message) {
 		var wroteAnswer = /wrote an answer/i;
 		var asked2answer = /asked you to answer/i;
-        
-        if(wroteAnswer.test(message) || asked2answer.test(message))
-            return true;
+		
+		if(wroteAnswer.test(message) || asked2answer.test(message))
+			return true;
 	}
     
     
-    // Send message to popup
+	// Send message to popup
 	chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-        if(request['isClassified'])
-        {
-            var response = {'isClassified': false};
-            
-            if(isClassified) {
-                response['isClassified'] = true;
-            }
-            sendResponse(resp);
-        }
-        
+		if(request['isClassified'])
+		{
+			var response = {'isClassified': false};
+			
+			if(isClassified) {
+				response['isClassified'] = true;
+			}
+			
+			sendResponse(resp);
+		}
+		
 		if(request['notifs'])
 		{
-            var response = { 'inbox': inbox, 'link': link, 'name': name, 'notifs': notifs, 'voteUp': voteUp, 'answered': answered, 'following': following, 'other': other };
+			var response = { 'inbox': inbox, 'link': link, 'name': name, 'notifs': notifs, 'voteUp': voteUp, 'answered': answered, 'following': following, 'other': other };
 			sendResponse(response);
 		}
 	});
-    
+	
 }());
